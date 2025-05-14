628531f3882aa92dc9e32f39429348889a55bc6c
Lorenzo <Lorenzo@DESKTOP-Q4EKPE5>
2025-05-14 13:10:58 -0300

    Adicionar o DAO para a classe Produto

---
+++ src/main/java/DAO/ProdutoDAO.java
@@ -0,0 +1,21 @@
+package DAO;
+
+import java.util.ArrayList;
+import model.Produto;
+
+/**
+ *
+ * @author Lorenzo
+ */
+public class ProdutoDAO {
+
+    private static ArrayList<Produto> listaProduto = new ArrayList<>();
+
+    public static ArrayList<Produto> getListaProduto() {
+        return listaProduto;
+    }
+
+    public static void setListaProduto(ArrayList listaProduto) {
+        ProdutoDAO.listaProduto = listaProduto;
+    }
+}
