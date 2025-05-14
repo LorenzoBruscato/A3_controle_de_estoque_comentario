33fbf04e2bbbb47f417211fd251c30887f166d5a
Lorenzo <Lorenzo@DESKTOP-Q4EKPE5>
2025-05-14 13:08:56 -0300

    Adiconar o DAO para a classe de Categoria

---
+++ src/main/java/DAO/CategoriaDAO.java
@@ -0,0 +1,25 @@
+package DAO;
+
+import java.util.ArrayList;
+
+import model.Categoria;
+
+/**
+ *
+ * @author Lorenzo
+ */
+public class CategoriaDAO {
+
+    public CategoriaDAO() {
+    }
+
+    private static ArrayList<Categoria> listaCategoria = new ArrayList<>();
+
+    public static ArrayList getListaCategoria() {
+        return listaCategoria;
+    }
+
+    public static void setListaCategoria(ArrayList<Categoria> listaCategoria) {
+        CategoriaDAO.listaCategoria = listaCategoria;
+    }
+}
