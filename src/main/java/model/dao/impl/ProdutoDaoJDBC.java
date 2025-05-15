1185ca110d285b7cf4df814b7f2c58124b5ae273
Lorenzo <Lorenzo@DESKTOP-Q4EKPE5>
2025-05-15 16:12:42 -0300

    Fix: Atualização do ProdutoDao implementação

---
+++ src/main/java/model/dao/impl/ProdutoDaoJDBC.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model.dao.impl;
+
+import java.util.List;
+import model.Produto;
+import model.dao.ProdutoDao;
+
+/**
+ *
+ * @author Lorenzo
+ */
+public class ProdutoDaoJDBC implements ProdutoDao{
+
+    @Override
+    public void cadastrarProduto(Produto obj) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void atualizarProduto(Produto obj) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void deletarProdutoPorId(int objId) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public List<Produto> resgatarProdutos() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
