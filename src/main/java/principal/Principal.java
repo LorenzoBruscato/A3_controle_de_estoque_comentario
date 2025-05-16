2baa02370ccde665302863571ea027b6fd501e4d
Lorenzo <Lorenzo@10.146.192.96>
2025-05-16 08:43:12 -0300

    Fix: Atualização do Principal

--- src/main/java/principal/Principal.java
+++ src/main/java/principal/Principal.java
@@ -1,15 +1,18 @@
 package principal;
 
 import view.FrmMenuPrincipal;
+import db.Database; // Importe a classe Database aqui
 
-/**
- *
- * @author Andradevh
- */
 public class Principal {
 
     public static void main(String[] args) {
-        FrmMenuPrincipal janela = new FrmMenuPrincipal();
-        janela.setVisible(true);
+        try {
+            Database.getConnection(); // Obtém a conexão com o banco de dados
+            FrmMenuPrincipal janela = new FrmMenuPrincipal();
+            janela.setVisible(true);
+        } catch (Exception e) {            
+        } finally {
+            Database.closeConnection(); // Fecha a conexão ao finalizar
+        }
     }
 }
