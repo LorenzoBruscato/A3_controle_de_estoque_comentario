8001a2c5c7a01489894ada7230e6c384e41f8e12
Lorenzo <Lorenzo@DESKTOP-Q4EKPE5>
2025-05-17 18:33:09 -0300

    Atualização do Principal

--- src/main/java/principal/Principal.java
+++ src/main/java/principal/Principal.java
@@ -1,5 +1,8 @@
 package principal;
 
+import db.Database;
+import model.dao.DaoFactory;
+import model.dao.ProdutoDao;
 import view.FrmMenuPrincipal;
 
 /**
@@ -8,10 +11,15 @@ import view.FrmMenuPrincipal;
  */
 public class Principal {
 
-public static void main(String[] args) {
-
+    public static void main(String[] args) {
+        try {
+            Database.getConnection();
+            ProdutoDao produtoDao = DaoFactory.instanciarProdutoDao();
             FrmMenuPrincipal janela = new FrmMenuPrincipal();
             janela.setVisible(true);
-
+        } catch (Exception e) {
+        } finally {
+            Database.closeConnection(); // Fecha a conexão ao finalizar
+        }
     }
 }
