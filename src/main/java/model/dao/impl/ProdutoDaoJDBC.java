38c0a9d8618e005acd5b663e51dfbac86db4d060
Lorenzo <Lorenzo@DESKTOP-Q4EKPE5>
2025-05-18 19:46:10 -0300

    Atualização ProdutoDaoJDBC

--- src/main/java/model/dao/impl/ProdutoDaoJDBC.java
+++ src/main/java/model/dao/impl/ProdutoDaoJDBC.java
@@ -7,6 +7,9 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import db.DbException;
+import java.util.HashMap;
+import java.util.Map;
+import model.Categoria;
 import model.Produto;
 import model.dao.ProdutoDao;
 
@@ -103,18 +106,27 @@ public class ProdutoDaoJDBC implements ProdutoDao {
     public List<Produto> resgatarProdutos() {
         List<Produto> lista = new ArrayList<>();
         String sql = "SELECT p.id AS idp, p.nome, p.preco, p.unidade, p.quantidade, "
-                + "p.quantidade_minima, p.quantidade_maxima, p.categoria_id, "
+                + "p.quantidade_minima, p.quantidade_maxima, p.categoria_id AS categoria_id, "
                 + "c.nome AS categoria_nome, c.tamanho, c.embalagem "
                 + "FROM produto p "
                 + "JOIN categoria c ON p.categoria_id = c.id "
                 + "ORDER BY p.nome";
 
         try (PreparedStatement st = conn.prepareStatement(sql); ResultSet rs = st.executeQuery()) {
+
+            List<Produto> list = new ArrayList<>();
+            Map<Integer, Categoria> map = new HashMap<>();
+            
             while (rs.next()) {
-                Produto prod = instantiateProduto(rs);
+                
+                Categoria cat = map.get(rs.getInt("Idc"));
+                if(cat == null){
+                    cat = instanciarCategoria(rs);
+                    map.put(rs.getInt("Idc"), cat);
+                }
+                Produto prod = instanciarProduto(rs, cat);
                 lista.add(prod);
             }
-
         } catch (SQLException e) {
             throw new DbException("Erro ao resgatar produtos: " + e.getMessage());
         }
@@ -122,7 +134,7 @@ public class ProdutoDaoJDBC implements ProdutoDao {
         return lista;
     }
 
-    private Produto instantiateProduto(ResultSet rs) throws SQLException {
+    private Produto instanciarProduto(ResultSet rs, Categoria cat) throws SQLException {
         Produto prod = new Produto();
         prod.setIdp(rs.getInt("idp"));
         prod.setNome(rs.getString("nome"));
@@ -131,6 +143,16 @@ public class ProdutoDaoJDBC implements ProdutoDao {
         prod.setQuantidade(rs.getInt("quantidade"));
         prod.setQuantidadeMinima(rs.getInt("quantidade_minima"));
         prod.setQuantidadeMaxima(rs.getInt("quantidade_maxima"));
+        prod.setCategoria(cat); // Adicionando a categoria ao produto
         return prod;
+    }
+
+    private Categoria instanciarCategoria(ResultSet rs) throws SQLException {
+        Categoria cat = new Categoria();
+        cat.setId(rs.getInt("categoria_id")); // Utiliza o alias correto do SELECT
+        cat.setNome(rs.getString("categoria_nome")); // Utiliza o alias correto do SELECT
+        cat.setTamanho(rs.getString("tamanho"));
+        cat.setEmbalagem(rs.getString("embalagem"));
+        return cat;
     }
 }
