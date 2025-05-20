335b36da94c1aa4441dd545d549efda8a28bb929
Lorenzo <Lorenzo@DESKTOP-Q4EKPE5>
2025-05-20 16:23:18 -0300

    Atualizando a Principal

--- src/main/java/principal/Principal.java
+++ src/main/java/principal/Principal.java
@@ -1,6 +1,6 @@
 package principal;
 
-import db.Database;
+import model.dao.CategoriaDao;
 import model.dao.DaoFactory;
 import model.dao.ProdutoDao;
 import view.FrmMenuPrincipal;
@@ -13,6 +13,7 @@ public class Principal {
 
     public static void main(String[] args) {
         
+            CategoriaDao categoriaDao = DaoFactory.instanciarCategoriaDao();
             ProdutoDao produtoDao = DaoFactory.instanciarProdutoDao();
             FrmMenuPrincipal janela = new FrmMenuPrincipal();
             janela.setVisible(true);
