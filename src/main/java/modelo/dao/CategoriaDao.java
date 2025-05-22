c6d8e1df2812cdb74e576404604da0bd60271bc0
Lorenzo <Lorenzo@DESKTOP-Q4EKPE5>
2025-05-22 09:59:23 -0300

    Atualizando CategoriaDao

--- src/main/java/modelo/dao/CategoriaDao.java
+++ src/main/java/modelo/dao/CategoriaDao.java
@@ -1,18 +1,20 @@
-
-
 package modelo.dao;
 
 import java.util.List;
 import modelo.Categoria;
 
 public interface CategoriaDao {
-    
+
     public void cadastrarCategoria(Categoria obj);
-    
+
     public void atualizarCategoria(Categoria obj);
-    
+
     public void deletarCategoriaPorId(int id);
-    
+
     public List<Categoria> resgatarCategorias();
-    
+
+    Categoria procurarCategoriaPorId(Integer id);
+
+    Categoria CategoriabuscarPorNome(String nome);
+
 }
