8f7e4ad2461c5ea7832c4982f503c4e516946aca
LorenzoBruscato <lorenzobruscato6@gmail.com>
2025-06-07 23:16:55 -0300

    FIX: Formatando e Comentando

--- src/main/java/principal/Principal.java
+++ src/main/java/principal/Principal.java
@@ -1,21 +1,20 @@
 package principal;
 
-import java.io.FileOutputStream;
-import java.util.List;
-import modelo.Categoria;
-import modelo.Produto;
-import modelo.dao.CategoriaDao;
-import modelo.dao.DaoFactory;
-import modelo.dao.ProdutoDao;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import visao.FrmMenuPrincipal;
 
+/**
+ * Classe principal que inicia a aplicação.
+ *
+ */
 public class Principal {
 
+    /**
+     * Método principal que executa a aplicação.
+     *
+     * @param args argumentos da linha de comando (não utilizados).
+     */
     public static void main(String[] args) {
-
         FrmMenuPrincipal janela = new FrmMenuPrincipal();
         janela.setVisible(true);
-
     }
 }
